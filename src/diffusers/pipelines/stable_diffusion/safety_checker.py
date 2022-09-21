4c43e597cb34d336d9decfc9dd07dcc108320697
Gregory D. Hunkins <ghunkins@u.rochester.edu>
2022-09-21 14:07:31 -0400

    ⬛ optional

--- src/diffusers/pipelines/stable_diffusion/safety_checker.py
+++ src/diffusers/pipelines/stable_diffusion/safety_checker.py
@@ -97,6 +97,4 @@ class StableDiffusionSafetyChecker(PreTrainedModel):
         # concept_scores = concept_scores.round(decimals=3)
         has_nsfw_concepts = torch.any(concept_scores > 0, dim=1)
 
-        images[has_nsfw_concepts] = 0.0  # black image
-
         return images, has_nsfw_concepts
